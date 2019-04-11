dedaf92fb7c02afa3e76ee3217fdfad489435f39
AlessandroSampaio <alessandrosampaio521@gmail.com>
2019-04-11 11:02:45 -0300

    Implementado bloqueio de ação caso logon não tenha sido realizado

--- CSC/Controllers/FuncionariosController.cs
+++ CSC/Controllers/FuncionariosController.cs
@@ -1,37 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CSC.Models;
+﻿using CSC.Models;
 using CSC.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CSC.Controllers
 {
     public class FuncionariosController : Controller
     {
         public readonly FuncionarioServices _funcionarioServices;
+        public readonly UserServices _userServices;
+        const string SessionUserID = "_UserID";
 
 
-        public FuncionariosController(FuncionarioServices funcionarioServices)
+        public FuncionariosController(FuncionarioServices funcionarioServices, UserServices userServices)
         {
             _funcionarioServices = funcionarioServices;
+            _userServices = userServices;
         }
 
-        public IActionResult Index()
-        {            
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.Session.GetInt32(SessionUserID).HasValue)
+            {
+                ViewBag.user = await _userServices.FindByIdAsync(HttpContext.Session.GetInt32(SessionUserID).Value);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
 
         public async Task<IActionResult> Listagem(string _name)
         {
-            if(_name == null) { _name = ""; }
+            if (_name == null) { _name = ""; }
             var listFuncionario = await _funcionarioServices.FindByNameAsync(_name);
             return PartialView("_listFuncionarios", listFuncionario);
         }
 
         public async Task<IActionResult> Editar(int id)
         {
+            if (HttpContext.Session.GetInt32(SessionUserID).HasValue)
+            {
+                ViewBag.user = await _userServices.FindByIdAsync(HttpContext.Session.GetInt32(SessionUserID).Value);
+            }
             Funcionario func = await _funcionarioServices.FindByIdAsync(id);
             return View(func);
         }
@@ -44,8 +57,12 @@ namespace CSC.Controllers
 
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            if (HttpContext.Session.GetInt32(SessionUserID).HasValue)
+            {
+                ViewBag.user = await _userServices.FindByIdAsync(HttpContext.Session.GetInt32(SessionUserID).Value);
+            }
             return View();
         }
 
---
+++ CSC/Controllers/UsuariosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSC.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public readonly UserServices _userServices;
+        const string SessionUserID = "_UserID";
+
+        public UsuariosController(UserServices userServices)
+        {
+            _userServices = userServices;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.Session.GetInt32(SessionUserID).HasValue)
+            {
+                ViewBag.user = await _userServices.FindByIdAsync(HttpContext.Session.GetInt32(SessionUserID).Value);
+            }
+            return View();
+        }
+    }
+}
