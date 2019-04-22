72b5e34a583bd92813933304cd5841f3cf34108c
AlessandroSampaio <alessandrosampaio521@gmail.com>
2019-04-22 17:44:42 -0300

    Adicionada validacao client-side

--- CSC/Models/Cliente.cs
+++ CSC/Models/Cliente.cs
@@ -7,23 +7,36 @@ namespace CSC.Models
     public class Cliente
     {
         public int Id { get; set; }
-        [Required]
-        [MaxLength(14)]
-        [MinLength(11)]
+
+        [Required(ErrorMessage = " ")]
+        [StringLength(14, MinimumLength = 11, ErrorMessage = "Tamanho inválido" )]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Somente números")]
         public string CNPJ { get; set; }
+
         [Required]
+        [Display(Name = "Razão Social")]
         public string RazaoSocial { get; set; }
+
         [Required]
+        [Display(Name = "Fantasia")]
         public string NomeFantasia { get; set; }
+
+        [Display(Name = "Data de Contrato")]
+        [DataType(DataType.Date)]
         public DateTime DataInicio { get; set; }
+
         public PessoaStatus Status { get; set; }
+
         public string Logradouro { get; set; }
+
         [StringLength(9,ErrorMessage ="CEP INVALIDO")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Somente números")]
         public string CEP { get; set; }
+
         public int Numero { get; set; }
+
         public string Bairro { get; set; }
+
         public string Cidade { get; set; }
     }
 }
